db14ab363ff0223f27b1f7554ffb413c4b77754b
sisitzky <90338095+sisitzky@users.noreply.github.com>
2021-12-09 11:52:54 -0500

    eda and data

---
+++ eda.R
@@ -0,0 +1,55 @@
+library(tidyverse)
+library(ggplot2)
+library(lme4)
+
+
+# read data and remove blank row
+df <- read.csv("stats.csv")
+df <- df[,-38]  
+
+# create babip variable
+df$b_babip <- (df$b_total_hits - df$b_home_run) / (df$b_ab - df$b_home_run - df$b_strikeout + df$b_total_sacrifices)  
+
+# creating subset without 2020 data
+df_no_2020 <- df %>%
+                subset(year != '2020')
+
+# exit velocity plot
+ggplot(df_no_2020, aes(x = log(exit_velocity_avg), y = log(b_babip), color = as.factor(year))) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+# creating launch angle subset
+la_df_no_2020 <- df_no_2020 %>%
+                  filter(launch_angle_avg > .5)
+
+# launch angle plot
+ggplot(la_df_no_2020, aes(x = log(launch_angle_avg), y = b_babip, color = as.factor(year))) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+# sweetspot % plot
+ggplot(df_no_2020, aes(x = log(sweet_spot_percent), y = log(b_babip), color = as.factor(year))) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+# barrel rate plot
+ggplot(df_no_2020, aes(x = log(barrel_batted_rate), y = log(b_babip), color = as.factor(year))) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+# oz contact % plot
+ggplot(df_no_2020, aes(x = log(oz_contact_percent), y = log(b_babip), color = as.factor(year))) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+# iz contact % plot
+ggplot(df_no_2020, aes(x = log(iz_contact_percent), y = log(b_babip), color = as.factor(year))) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+model <- lmer(b_babip ~ sprint_speed + exit_velocity_avg + launch_angle_avg + sweet_spot_percent + (1 + exit_velocity_avg|year) + (1 + launch_angle_avg|year) + (1 + sweet_spot_percent|year), data = df_no_2020)
+summary(model)
+
+
+
